7a3a48370dca294d133f6ba4b43988b5d82784d6
Jovita <jovitaspais@gmail.com>
2022-02-01 17:41:51 +0000

    Big Bang: finish exercise 1-7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store
+  validates :first_name, :last_name, :presence => true
+  validates :hourly_rate, length: {within: 40..200}
+  validates :store, :presence => true
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { :only_integer => true }
+  validate :atleast_one_apparel
 
+  def atleast_one_apparel
+    
+     if mens_apparel == nil && womens_apparel == nil
+        errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
+        errors.add(:womens_apparel, "Stores must carry at least one of women's apparel")
+      
+     end
+  end
 end
+
